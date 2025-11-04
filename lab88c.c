c53535fc5bf929177163e3bd39f7ffc51aa0329b
StepanGusev251 <stepangusev641@gmail.com>
2025-11-04 22:40:29 +0300

    Add files via upload

---
+++ lab88c.c
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+#define _CRT_SECURE_NO_DEPRECATE
+#include <stdlib.h>
+#include <locale.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+int main() {
+    int a, n;
+    long long product = 1;
+    setlocale(LC_ALL, "RUS");
+    // Ввод значений a и n
+    printf("Введите значение a: ");
+    scanf("%d", &a);
+    printf("Введите значение n: ");
+    scanf("%d", &n);
+
+    // Вычисление произведения ряда
+    for (int i = 0; i <= n; i++) {
+        product = product * (a - i * n);
+    }
+
+    // Вывод результата
+    printf("Произведение ряда: %lld\n", product);
+
+    return 0;
+}
